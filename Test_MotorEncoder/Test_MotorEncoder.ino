a20e365af925fe5d2327050ec07f1a16f026d7d6
Anton Lyngfelt <anton.lyngfelt@hotmail.com>
2019-09-05 16:18:43 +0200

    Some Changes but they are wrong

--- Test_MotorEncoder/Test_MotorEncoder.ino
+++ Test_MotorEncoder/Test_MotorEncoder.ino
@@ -1,18 +1,25 @@
 float encA;
 float encB;
 
-//Motor Pins
-int EN_A = 11;      //Enable pin for first motor
-int IN1 = 9;       //control pin for first motor
-int IN2 = 8;       //control pin for first motor
-//int IN3 = 7;        //control pin for second motor
-//int IN4 = 6;        //control pin for second motor
-//int EN_B = 10;      //Enable pin for second motor
+//Motor Pins in heading direction
+int EN_A_RIGHTMOTOR = 13;      //Enable pin for right motor
+int IN1_RIGHTMOTOR = 12;       //control pin for right motor
+int IN2_RIGHTMOTOR = 11;       //control pin for right motor
 
-//Encoder pins
-int VCC = 2;
-int outA = 4;
-int outB = 5;
+int EN_B_LEFTMOTOR = 10;      //Enable pin for left motor
+int IN3_EN_B_LEFTMOTOR = 9;        //control pin for left motor
+int IN4_EN_B_LEFTMOTOR = 8;        //control pin for left motor
+
+
+//EncoderA pins
+int VCC_RIGHTMOTOR = 5;
+int OUT_A_RIGHTMOTOR = 6;
+int OUT_B_RIGHTMOTOR = 7;
+
+//EncoderB pins
+int VCC_LEFTMOTOR = 2;
+int OUT_A_LEFTMOTOR = 3;
+int OUT_B_LEFTMOTOR = 4;
 
 //Initializing variables to store data
 int motor_speedL;
@@ -29,39 +36,42 @@ void setup ( ) {
   Serial.begin (9600); //Starting the serial communication at 9600 baud rate
   //Initializing the motor pins as output
 
-  //Left Motor
-  pinMode(EN_A, OUTPUT);
-  pinMode(IN1, OUTPUT);  
-  pinMode(IN2, OUTPUT);
   //Right Motor
-  /*pinMode(EN_B, OUTPUT);
-  pinMode(IN3, OUTPUT);  
-  pinMode(IN4, OUTPUT);
-  */
-  //Left Encoder 
-  pinMode(VCC, OUTPUT);
-  pinMode(outA, INPUT);
-  pinMode(outB, INPUT);
-  
- 
+  pinMode(EN_A_RIGHTMOTOR, OUTPUT);
+  pinMode(IN1_RIGHTMOTOR, OUTPUT);
+  pinMode(IN2_RIGHTMOTOR, OUTPUT);
+  //Left Motor
+  pinMode(EN_B_LEFTMOTOR, OUTPUT);
+  pinMode(IN3_EN_B_LEFTMOTOR, OUTPUT);
+  pinMode(IN4_EN_B_LEFTMOTOR, OUTPUT);
+
+  //Right Encoder
+  pinMode(VCC_RIGHTMOTOR, OUTPUT);
+  pinMode(OUT_A_RIGHTMOTOR, INPUT);
+  pinMode(OUT_B_RIGHTMOTOR, INPUT);
+
+  //Left Encoder
+  pinMode(VCC_LEFTMOTOR, OUTPUT);
+  pinMode(OUT_A_LEFTMOTOR, INPUT);
+  pinMode(OUT_B_LEFTMOTOR, INPUT);
+
+
   //Setting Direction of motors according to L298N motor controller
-  // Left Motor 
+  // Left Motor
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  
-  // Right motor 
-  //digitalWrite(IN3, HIGH);
-  //digitalWrite(IN4, LOW);
 
-  //Enable encoder 
-  digitalWrite(VCC, HIGH);
-  
-  
-           
+  // Right motor
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  //Enable encoders
+  digitalWrite(VCC_RIGHTMOTOR, HIGH);
+  digitalWrite(VCC_LEFTMOTOR, HIGH);
 }
 
-void loop() 
-{       
+void loop()
+{
     motor_speedL = maxspeed;
     //motor_speedR = maxspeed;
     analogWrite(EN_A, motor_speedL);
@@ -72,7 +82,7 @@ void loop()
     Serial.println(encB);
     //analogWrite(EN_B, motor_speedR);
     delay(10);
-    
+
     //motor_speedL = stopspeed;
     //motor_speedR = stopspeed;
     //analogWrite(EN_A, motor_speedL);
