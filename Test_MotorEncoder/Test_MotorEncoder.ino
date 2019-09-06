ff4d593b071b37f615d6ca4b51fd804fe8d8f56e
Anton Lyngfelt <anton.lyngfelt@hotmail.com>
2019-09-06 11:50:25 +0200

    Code for testing both engines upploaded, works

--- Test_MotorEncoder/Test_MotorEncoder.ino
+++ Test_MotorEncoder/Test_MotorEncoder.ino
@@ -7,11 +7,11 @@ int IN1_RIGHTMOTOR = 12;       //control pin for right motor
 int IN2_RIGHTMOTOR = 11;       //control pin for right motor
 
 int EN_B_LEFTMOTOR = 10;      //Enable pin for left motor
-int IN3_EN_B_LEFTMOTOR = 9;        //control pin for left motor
-int IN4_EN_B_LEFTMOTOR = 8;        //control pin for left motor
+int IN3_LEFTMOTOR = 9;        //control pin for left motor
+int IN4_LEFTMOTOR = 8;        //control pin for left motor
 
 
-//EncoderA pins
+/*//EncoderA pins
 int VCC_RIGHTMOTOR = 5;
 int OUT_A_RIGHTMOTOR = 6;
 int OUT_B_RIGHTMOTOR = 7;
@@ -20,12 +20,12 @@ int OUT_B_RIGHTMOTOR = 7;
 int VCC_LEFTMOTOR = 2;
 int OUT_A_LEFTMOTOR = 3;
 int OUT_B_LEFTMOTOR = 4;
-
+*/
 //Initializing variables to store data
 int motor_speedL;
 int motor_speedR;
 // Set speeds accordingly 0-min, 255-max
-int maxspeed = 0; //Max is 255
+int maxspeed = 50; //Max is 255
 int stopspeed = 0;
 
 
@@ -42,9 +42,9 @@ void setup ( ) {
   pinMode(IN2_RIGHTMOTOR, OUTPUT);
   //Left Motor
   pinMode(EN_B_LEFTMOTOR, OUTPUT);
-  pinMode(IN3_EN_B_LEFTMOTOR, OUTPUT);
-  pinMode(IN4_EN_B_LEFTMOTOR, OUTPUT);
-
+  pinMode(IN3_LEFTMOTOR, OUTPUT);
+  pinMode(IN4_LEFTMOTOR, OUTPUT);
+  /*
   //Right Encoder
   pinMode(VCC_RIGHTMOTOR, OUTPUT);
   pinMode(OUT_A_RIGHTMOTOR, INPUT);
@@ -55,32 +55,37 @@ void setup ( ) {
   pinMode(OUT_A_LEFTMOTOR, INPUT);
   pinMode(OUT_B_LEFTMOTOR, INPUT);
 
+  */
 
   //Setting Direction of motors according to L298N motor controller
-  // Left Motor
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
+  // Right Motor
+  digitalWrite(IN1_RIGHTMOTOR, HIGH);
+  digitalWrite(IN2_RIGHTMOTOR, LOW);
 
-  // Right motor
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  // Left motor
+  digitalWrite(IN3_LEFTMOTOR, HIGH);
+  digitalWrite(IN4_LEFTMOTOR, LOW);
 
+  /*
   //Enable encoders
   digitalWrite(VCC_RIGHTMOTOR, HIGH);
   digitalWrite(VCC_LEFTMOTOR, HIGH);
+  */
 }
 
 void loop()
 {
     motor_speedL = maxspeed;
-    //motor_speedR = maxspeed;
-    analogWrite(EN_A, motor_speedL);
+    motor_speedR = maxspeed;
+    analogWrite(EN_A_RIGHTMOTOR, motor_speedL);
+    /*
     encA = digitalRead(outA);
     encB = digitalRead(outB);
     Serial.print(encA);
     Serial.print(' ');
     Serial.println(encB);
-    //analogWrite(EN_B, motor_speedR);
+    */
+    analogWrite(EN_B_LEFTMOTOR, motor_speedR);
     delay(10);
 
     //motor_speedL = stopspeed;
